474d4faf7ddfe54ff42c24982325ceb4a716db2a
Grace Odongo <graceakinyi024@gmail.com>
2026-02-01 06:21:46 +0300

    add category

--- server/resources/admin.py
+++ server/resources/admin.py
@@ -1,20 +1,29 @@
+from flask import request
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from models import db, Project, User
 
-class ApproveProject(Resource):
+from models import db, User, Project, Category, UserProject
+
+#adding category
+class CategoryCreate(Resource):
     @jwt_required()
-    def post(self, project_id):
+    def post(self):
         current_user_id = get_jwt_identity()
         user = User.query.get(current_user_id)
 
         if user.role.name != "admin":
-            return {"error": "Only admin can approve projects"}, 403
+            return {"error": "Only admin can add categories"}, 403
 
-        project = Project.query.get(project_id)
-        if not project:
-            return {"error": "Project not found"}, 404
+        data = request.get_json()
+        if Category.query.filter_by(name=data["name"]).first():
+            return {"error": "Category already exists"}, 400
 
-        project.status = "approved"
+        category = Category(
+            name=data["name"],
+            description=data.get("description", "")
+        )
+        db.session.add(category)
         db.session.commit()
-        return {"message": f"Project {project.title} approved"}, 200
+        return {"message": f"Category '{category.name}' created"}, 201
+
+
