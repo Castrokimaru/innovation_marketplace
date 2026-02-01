260e0efb415262fa5cbb33d85244f674f40c2ad4
Grace Odongo <graceakinyi024@gmail.com>
2026-02-01 06:03:43 +0300

    remove unnecessary lines of code

--- server/resources/merchandise.py
+++ server/resources/merchandise.py
@@ -1,33 +1,3 @@
-# from flask_restful import Resource
-# from flask import request
-# from models import db, Merchandise
-
-# class MerchandiseList(Resource):
-#     def post(self):
-#         data = request.get_json()
-
-#         item = Merchandise(
-#             name=data["name"],
-#             description=data["description"],
-#             price=data["price"],
-#             stock=data["stock"],
-#             image_url=data["image_url"]
-#         )
-#         db.session.add(item)
-#         db.session.commit()
-
-#         return {"message": "Merchandise added"}, 201
-
-#     def get(self):
-#         items = Merchandise.query.all()
-#         return [
-#             {
-#                 "id": m.id,
-#                 "name": m.name,
-#                 "price": float(m.price),
-#                 "stock": m.stock
-#             } for m in items
-#         ], 200
 from flask import request
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required
