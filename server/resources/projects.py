08feb7dc5fca949474690a1d2dbfff27ede2f3db
Grace Odongo <graceakinyi024@gmail.com>
2026-02-01 09:45:03 +0300

    create project with contributors added to user_project table

--- server/resources/projects.py
+++ server/resources/projects.py
@@ -2,37 +2,29 @@ from flask import request
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from models import db, User, Project, UserProject, Category, ProjectCategory
-
+from models import db, Project, UserProject, ProjectCategory, User, Category
 
 class ProjectList(Resource):
-#GET all projects with their contributors
     def get(self):
         projects = Project.query.all()
         result = []
 
         for p in projects:
-            # Getting all users linked to this project
-            contributors = (
-                User.query
-                .join(UserProject)
-                .filter(UserProject.project_id == p.id)
-                .all()
-            )
-
+            # Team members
             team = [
                 {
-                    "id": u.id,
-                    "first_name": u.first_name,
-                    "last_name": u.last_name,
-                    "email": u.email,
-                    "role": u.role.name
-                } for u in contributors
+                    "id": up.user.id,
+                    "first_name": up.user.first_name,
+                    "last_name": up.user.last_name,
+                    "email": up.user.email,
+                    "role": up.user.role.name
+                } for up in p.users
             ]
-            # Get categories
+
+            # Categories
             cats = [
-                {"id": c.category.id, "name": c.category.name}
-                for c in p.categories
+                {"id": pc.category.id, "name": pc.category.name}
+                for pc in p.categories if pc.category
             ]
 
             result.append({
@@ -49,77 +41,63 @@ class ProjectList(Resource):
             })
 
         return result, 200
-        # return [
-        #     {
-        #         "id": p.id,
-        #         "title": p.title,
-        #         "status": p.status,
-        #         "technologies": p.technologies
-        #     } for p in projects
-        # ], 200
 
     @jwt_required()
-# Creating a project with creator + contributors
     def post(self):
-        user_id = int(get_jwt_identity())
+        user_id = get_jwt_identity()
         data = request.get_json()
 
-        # category_ids = data.get("category_ids", [])  # list of category IDs
-        # categories = []
-        # for cid in category_ids:
-        #     cat = Category.query.get(cid)
-        #     if not cat:
-        #         return {"error": f"Category id {cid} not found"}, 400
-        # categories.append(cat)
+        title = data.get("title")
+        description = data.get("description")
+        video = data.get("video")
+        technologies = data.get("technologies")
+        submitted_name = data.get("submitted_name")
+        team_members = data.get("team_members", [])
+        category_ids = data.get("category_ids", [])
 
+        if not all([title, description, video, technologies, submitted_name]):
+            return {"error": "Missing required fields"}, 400
+
+        # Create project
         project = Project(
-            title=data["title"],
-            description=data["description"],
-            video=data["video"],
-            technologies=data["technologies"],
-            submitted_name=data["submitted_name"],
+            title=title,
+            description=description,
+            video=video,
+            technologies=technologies,
+            submitted_name=submitted_name,
         )
-
         db.session.add(project)
         db.session.commit()
-#linking the project to the creator
-        db.session.add(UserProject(
+
+        # Link creator
+        creator_link = UserProject(
             user_id=user_id,
             project_id=project.id,
             action="creator"
-        ))
-#linking team members
-        for member_id in data.get("team_members", []):
-            # skip if user_id == creator
+        )
+        db.session.add(creator_link)
+
+        # Link team members (skip creator)
+        for member_id in team_members:
             if member_id == user_id:
                 continue
+            user = User.query.get(member_id)
+            if user:
+                db.session.add(UserProject(
+                    user_id=user.id,
+                    project_id=project.id,
+                    action="contributor"
+                ))
 
-            member = User.query.get(member_id)
-            if not member:
-                return {"error": f"User id {member_id} not found"}, 400
-        
-            db.session.add(UserProject(
-                user_id=member_id,
-                project_id=project.id,
-                action="contributor"
-            ))
-#link categories
-        for cid in data.get("category_ids", []):
-            cat = Category.query.get(cid)
-            if not cat:
-                return {"error": f"Category id {cid} not found"}, 400
-            db.session.add(ProjectCategory(
-                project_id=project.id,
-                category_id=cat.id
-            ))
-
-        # db.session.add(link)
-        # for cat in categories:
-        #     project_category_link = ProjectCategory(
-        #         project_id=project.id,
-        #         category_id=cat.id
-        #     )
-        # db.session.add(project_category_link)
+        # Link categories
+        for cat_id in category_ids:
+            category = Category.query.get(cat_id)
+            if category:
+                db.session.add(ProjectCategory(
+                    project_id=project.id,
+                    category_id=category.id
+                ))
 
         db.session.commit()
-        return {"message": "Project submitted"}, 201
+
+        return {"message": "Project created", "project_id": project.id}, 201
