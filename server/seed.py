2994d41b3e1ab789bbb631deab24d3f19286f141
Grace Odongo <graceakinyi024@gmail.com>
2026-02-04 23:37:00 +0300

    add users

--- server/seed.py
+++ server/seed.py
@@ -31,12 +31,57 @@ def seed_roles():
 
     db.session.commit()
 
+def seed_users():
+    admin_role = UserRole.query.filter_by(name="admin").first()
+    student_role = UserRole.query.filter_by(name="student").first()
+    recruiter_role = UserRole.query.filter_by(name="recruiter").first()
 
+    admin = User(
+        first_name="Fred",
+        last_name="Chen",
+        email="admin@moringa.co.ke",
+        password_hash=generate_password_hash("Admin1234"),
+        role_id=admin_role.id,
+        status="active",
+        created_at=datetime.utcnow(),
+    )
+    db.session.add(admin)
+
+    students = []
+    for _ in range(15):
+        student = User(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            email=fake.unique.email(),
+            password_hash=generate_password_hash("Student1234"),
+            role_id=student_role.id,
+            status="active",
+        )
+        students.append(student)
+        db.session.add(student)
+
+    for _ in range(5):
+        db.session.add(
+            User(
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                email=fake.unique.email(),
+                password_hash=generate_password_hash("Recruiter1234"),
+                role_id=recruiter_role.id,
+                status="active",
+            )
+        )
+
+    db.session.commit()
+    return students   
 
 if __name__ == "__main__":
     with app.app_context():
         print("Cleared database")
         clear_db()
 
-        print("Seeding roles")
+        print("Seed roles")
         seed_roles()
+
+        print("Seed users")
+        students = seed_users()
