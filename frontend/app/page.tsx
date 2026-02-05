edda769575a627ae9f5c5f2764505e21acf5c56e
Grace Odongo <graceakinyi024@gmail.com>
2026-02-06 00:00:38 +0300

    added merchandise section route

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,6 +7,7 @@ import { WhyChooseUs } from '@/components/why-choose-us'
 import { CTASection } from '@/components/cta-section'
 import { Footer } from '@/components/footer'
 import { SessionProvider } from "next-auth/react"
+import { MerchandiseSection } from '@/components/merchandise-section'
 
 export default function Home({session}:any) {
   return (
@@ -16,6 +17,7 @@ export default function Home({session}:any) {
       <main className="overflow-hidden">
         <Hero />
         <FeaturedProjects />
+        <MerchandiseSection/>
         <HowItWorks />
         <WhyChooseUs />
         <CTASection />
