ae4c3e461eabf9da0af166b49af70c2be355771f
Grace Odongo <graceakinyi024@gmail.com>
2026-02-05 23:58:51 +0300

    added get started button to direct user to sign in

--- frontend/components/hero.tsx
+++ frontend/components/hero.tsx
@@ -43,14 +43,14 @@ export function Hero() {
               <Button 
                 size="lg" 
                 className="w-full sm:w-auto bg-primary hover:bg-primary/90"
-                onClick={handleExploreProjects}
-              >
-                Explore Projects
+                onClick={() => router.push('./auth/signin')}
+                >
+                  Get Started
                 <ArrowRight className="ml-2 h-4 w-4" />
               </Button>
-              <Button size="lg" variant="outline" className="w-full sm:w-auto bg-transparent border-white text-white hover:bg-white hover:text-black">
+              {/* <Button size="lg" variant="outline" className="w-full sm:w-auto bg-transparent border-white text-white hover:bg-white hover:text-black">
                 Learn More
-              </Button>
+              </Button> */}
             </div>
           </div>
           <div className="relative">
