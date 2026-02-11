afb0e5c2512c22be47fbf1f29879dc21e2677e3f
Grace Odongo <graceakinyi024@gmail.com>
2026-02-11 11:08:27 +0300

    add postgres path

--- server/app.py
+++ server/app.py
@@ -24,7 +24,11 @@ from resources.users import UserList
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "postgresql+psycopg2://odongo:odongo1234@localhost:5432/app_db"
+)
+
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "dev-secret-key")
 
