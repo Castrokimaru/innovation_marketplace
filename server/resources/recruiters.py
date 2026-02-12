6b16808d9cab6c7cbbe4852ea301d58ddb941ffc
joshuaimbusi <joshua.imbusi@student.moringaschool.com>
2026-02-12 19:01:52 +0300

    feat(recruiters): enhance recruiter project browsing

--- server/resources/recruiters.py
+++ server/resources/recruiters.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from models import Project, UserProject, User
+from models import Project
 
 
 class BrowseProjects(Resource):
@@ -12,13 +12,15 @@ class BrowseProjects(Resource):
                 {"id": link.user.id, "name": f"{link.user.first_name} {link.user.last_name}"}
                 for link in p.users
             ]
+
             result.append({
                 "id": p.id,
                 "title": p.title,
                 "description": p.description,
                 "technologies": p.technologies,
                 "submitted_name": p.submitted_name,
-                "team_members": team_members
+                "team_members": team_members,
+                "thumbnail_url": p.thumbnail_url,
             })
 
         return result, 200
